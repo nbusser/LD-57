ea9185e4ddf50fcf89b9a51fb682ad0342659a27
Antoine Brûlé <antoine.brule@netc.eu>
2025-04-06 01:25:39 +0200

    add more enemy animations

---
+++ src/Enemy/enemy.gd
@@ -0,0 +1,34 @@
+extends Node3D
+
+enum EnemyType {SPROINK, PIG}
+enum EnemyState {IDLE, WEAK, DISTRACTED}
+
+@onready var animated_sprite_3d: AnimatedSprite3D = $AnimatedSprite3D
+
+var type: EnemyType = EnemyType.SPROINK:
+	set(value):
+		type = value
+		_update_sprite()
+var state: EnemyState = EnemyState.IDLE:
+	set(value):
+		state = value
+		_update_sprite()
+
+func _update_sprite():
+	match [type, state]:
+		# sproink
+		[EnemyType.SPROINK, EnemyState.IDLE]:
+			animated_sprite_3d.play('sproink_idle')
+		[EnemyType.SPROINK, EnemyState.WEAK]:
+			animated_sprite_3d.play('sproink_weak')
+		[EnemyType.SPROINK, ..]:
+			animated_sprite_3d.play('sproink_idle')
+		# pig
+		[EnemyType.PIG, EnemyState.DISTRACTED]:
+			animated_sprite_3d.play('pig_distracted')
+		[EnemyType.PIG, ..]:
+			animated_sprite_3d.play('pig_distracted')
+	
+
+func _ready() -> void:
+	_update_sprite()
