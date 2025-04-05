f2605de15663ee60bbdfad4d9546d563a56e48a8
mbty <mbty@protonmail.com>
2025-04-05 12:32:44 +0200

    disable horizontal panning

--- src/Map/camera.gd
+++ src/Map/camera.gd
@@ -7,10 +7,6 @@ extends Camera3D
 func _physics_process(delta: float) -> void:
 	var x_ratio = get_viewport().get_mouse_position().x/get_viewport().get_size().x
 	var y_ratio = get_viewport().get_mouse_position().y/get_viewport().get_size().y
-	if x_ratio < margin_prop:
-		rotation += Vector3(0, speed, 0)
-	elif x_ratio > (1.0 - margin_prop):
-		rotation -= Vector3(0, speed, 0)
 	if y_ratio < margin_prop:
 		rotation += Vector3(speed, 0, 0)
 	elif y_ratio > (1.0 - margin_prop):
