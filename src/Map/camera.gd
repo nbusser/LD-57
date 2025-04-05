5e609b511447687edb002767d2c117dc515f425d
mbty <mbty@protonmail.com>
2025-04-05 12:28:21 +0200

    clamp camera

--- src/Map/camera.gd
+++ src/Map/camera.gd
@@ -3,6 +3,8 @@ extends Camera3D
 func _physics_process(delta: float) -> void:
 	const margin_prop = .1
 	const speed = .008
+	const amplitude = 20 # degrees
+	
 	var x_ratio = get_viewport().get_mouse_position().x/get_viewport().get_size().x
 	var y_ratio = get_viewport().get_mouse_position().y/get_viewport().get_size().y
 	if x_ratio < margin_prop:
@@ -13,3 +15,7 @@ func _physics_process(delta: float) -> void:
 		rotation += Vector3(speed, 0, 0)
 	elif y_ratio > (1.0 - margin_prop):
 		rotation -= Vector3(speed, 0, 0)
+	rotation = rotation.clamp(
+		Vector3(deg_to_rad(-20 - amplitude), deg_to_rad(-amplitude), 0),
+		Vector3(deg_to_rad(-20 + amplitude), deg_to_rad(amplitude), 0)
+	)
