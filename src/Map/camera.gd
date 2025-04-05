4ac26021d9ddafb668d05f660b60fc1278ee064e
mbty <mbty@protonmail.com>
2025-04-05 12:57:23 +0200

    add camera rail

--- src/Map/camera.gd
+++ src/Map/camera.gd
@@ -1,17 +1,25 @@
 extends Camera3D
 
 @export var margin_prop = .1
-@export var speed = .008
+@export var speed = 1
 @export var amplitude = 20 # degrees
 
+@onready var rail = get_node("..")
+
 func _physics_process(delta: float) -> void:
-	var x_ratio = get_viewport().get_mouse_position().x/get_viewport().get_size().x
 	var y_ratio = get_viewport().get_mouse_position().y/get_viewport().get_size().y
 	if y_ratio < margin_prop:
-		rotation += Vector3(speed, 0, 0)
+		rotation += Vector3(speed*delta, 0, 0)
 	elif y_ratio > (1.0 - margin_prop):
-		rotation -= Vector3(speed, 0, 0)
+		rotation -= Vector3(speed*delta, 0, 0)
 	rotation = rotation.clamp(
 		Vector3(deg_to_rad(-20 - amplitude), deg_to_rad(-amplitude), 0),
 		Vector3(deg_to_rad(-20 + amplitude), deg_to_rad(amplitude), 0)
 	)
+
+func _on_weeeee_toggled(toggled_on: bool) -> void:
+	var tween = get_tree().create_tween()
+	if toggled_on:
+		tween.tween_property(rail, "progress_ratio", 0.99, 1)
+	else:
+		tween.tween_property(rail, "progress_ratio", 0.01, 1)
