803d1b681eb87c66cfb3efff2c23c6bff63ead90
caimez323 <caimezae@gmail.com>
2025-04-06 12:35:29 +0200

    add of state machine for card game logic

--- src/cardgame/cardgame.gd
+++ src/cardgame/cardgame.gd
@@ -1,16 +1,37 @@
 class_name CardGame
 extends Node
 
+@onready var playerDeckNode = get_node("../deckManager/deckObjectPlayer")
+@onready var alienDeckNode = get_node("../deckManager/deckObjectAlien")
+@onready var cardScene = preload("res://src/Card/Card.tscn")
+
+var roundManager = null
+
+enum GameState {
+	NOT_STARTED,
+	INIT,
+	DRAW,
+	WAITING_FOR_RECUP,
+	WHOS_FIRST,
+	PLAYER_TURN,
+	ALIEN_TURN,
+	BATTLE,
+	GAME_OVER,
+	WIN
+}
+var current_state: GameState = GameState.NOT_STARTED
+var precedent_state: GameState = current_state
 
 class RoundManager:
 	const DECK_SIZE: int = 21
 	const HAND_SIZE: int = 3
 	const DEFAULT_DECK: Array = [1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4, 5, 5, 5, 0, 0, 0, -2, -2, -2]
+	var myCustomDeck : Array = []
 	var my_deck: Array = []
 	var alien_deck: Array = []
-	#Sur le batteField c'est : {"player": "player", "card": 1}
-	var battle_field: Array = []
-	var first_player: bool = false
+	#Sur le battleField c'est : {"player": "player", "card": 1}
+	var battleField: Array = []
+	var first_player: bool = true
 	var alien_hand: Array = []
 	var alien_life: int = 20
 	var player_life: int = 20
@@ -24,12 +45,12 @@ class RoundManager:
 			my_deck = player_deck_arg.duplicate()
 		#On setup le deck de l'alien
 		if alien_deck_arg.size() == 0:
-			alien_deck_arg = DEFAULT_DECK.duplicate()
+			alien_deck = DEFAULT_DECK.duplicate()
 		else:
-			alien_deck_arg = alien_deck_arg.duplicate()
+			alien_deck = alien_deck_arg.duplicate()
 		#On mélange les decks
 		my_deck.shuffle()
-		alien_deck_arg.shuffle()
+		alien_deck.shuffle()
 
 	#Fonciton pour piocher X cartes
 	func draw_cards(deck: Array, count: int) -> Array:
@@ -44,7 +65,7 @@ class RoundManager:
 
 	# Fonction pour jouer une carte sur le champ de bataille
 	func play_card(player: String, card: int) -> void:
-		battle_field.append({"player": player, "card": card})
+		battleField.append({"player": player, "card": card})
 
 	# Fonction pour faire piocher l'alien à partir de la fonction draw_cards et de alien_hand
 	func alien_draw_cards(count: int) -> void:
@@ -58,8 +79,8 @@ class RoundManager:
 	#Résolution du combat
 	func battle() -> bool:
 		var was_first = first_player
-		var player_score = battle_field[0]["card"] if battle_field[0]["player"] == "player" else 0
-		var alien_score = battle_field[0]["card"] if battle_field[0]["player"] == "alien" else 0
+		var player_score = battleField[0]["card"] if battleField[0]["player"] == "player" else 0
+		var alien_score = battleField[0]["card"] if battleField[0]["player"] == "alien" else 0
 
 		#CAS SPECIAL ETOILE ET -2
 		if alien_score == 0 or player_score == 0:
@@ -110,3 +131,91 @@ class RoundManager:
 
 	#Trouver un moyen de detecter la fin de partie.
 	#Toutes les données sont accessible depuis la classe la
+
+
+func _instantiate_card(cardArg: PackedScene, posReferenceNode: Node3D ,value : int) -> Node3D:
+	#On instancie la carte
+	var cardInst = cardArg.instantiate()
+	cardInst.global_position = posReferenceNode.global_position
+	cardInst.init(value)
+	add_child(cardInst)
+	return cardInst
+
+
+func _ready() -> void:
+	#Pour le débug on start le round mtn
+	_start_game()
+	return
+
+func _start_game () -> void:
+	roundManager = RoundManager.new()
+	current_state = GameState.INIT
+	
+func _process(delta: float) -> void:
+	if current_state == GameState.NOT_STARTED:
+		return
+
+	if current_state == GameState.INIT:
+		roundManager.setup_decks(roundManager.myCustomDeck)
+		for i in range(RoundManager.HAND_SIZE-1):
+			var card = roundManager.draw_cards(roundManager.my_deck, 1)
+			_instantiate_card(cardScene, playerDeckNode, card[0])
+		
+		for i in range(RoundManager.HAND_SIZE-1):
+			var card = roundManager.draw_cards(roundManager.alien_deck, 1)
+			_instantiate_card(cardScene, alienDeckNode, card[0])
+
+		#La distrubution des cartes est faite, on passe en DRAW
+		current_state = GameState.DRAW
+
+	if current_state == GameState.DRAW:
+		var card = TYPE_NIL
+		card = roundManager.draw_cards(roundManager.my_deck, 1)
+		_instantiate_card(cardScene, playerDeckNode, card[0])
+		card = roundManager.draw_cards(roundManager.alien_deck, 1)
+		_instantiate_card(cardScene, alienDeckNode, card[0])
+		current_state = GameState.WAITING_FOR_RECUP
+
+	if current_state == GameState.WAITING_FOR_RECUP:
+		#On attend que le joueur prenne les cartes, ici on met direct le bon truc
+		#if blablabla
+	
+		current_state = GameState.WHOS_FIRST
+		
+	if current_state == GameState.WHOS_FIRST:
+		if roundManager.first_player:
+			current_state = GameState.PLAYER_TURN
+		else:
+			current_state = GameState.ALIEN_TURN
+
+	if current_state == GameState.PLAYER_TURN:
+		#Faudra configurer ici le fait de poser une carte et tout
+		for card in roundManager.battleField:
+			#On vérifie qu'on a bien posé la carte
+			if card["player"] == "player":
+				roundManager.play_card("player", card["card"])
+				if roundManager.first_player:
+					current_state = GameState.ALIEN_TURN
+				else:
+					current_state = GameState.BATTLE
+
+	if current_state == GameState.ALIEN_TURN:
+		#Faudra configurer ici le fait de poser une carte et tout
+		for card in roundManager.battleField:
+			#On vérifie qu'on a bien posé la carte
+			if card["player"] == "alien":
+				roundManager.play_card("alien", card["card"])
+				if roundManager.first_player:
+					current_state = GameState.PLAYER_TURN
+				else:
+					current_state = GameState.BATTLE
+
+	if current_state == GameState.BATTLE:
+		#On fait la bataille
+		roundManager.battle()
+		if roundManager.player_life <= 0:
+			current_state = GameState.GAME_OVER
+		elif roundManager.alien_life <= 0:
+			current_state = GameState.WIN
+		else:
+			current_state = GameState.DRAW
