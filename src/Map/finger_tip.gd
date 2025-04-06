da98b8cd354e5b86f30deefc2ce23a1ac7b5b7b3
Nicolas Busser <nicolas.busser67@gmail.com>
2025-04-06 16:59:42 +0900

    fix raycast

--- src/Map/finger_tip.gd
+++ src/Map/finger_tip.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+const CARD_LAYER = 7
+
 @onready var camera = $"../../../../CameraRail/FollowRail/Camera"
 
 
@@ -9,6 +11,7 @@ func get_closest_card() -> Card:
 	var ray_query = PhysicsRayQueryParameters3D.new()
 	ray_query.from = camera.project_ray_origin(mouse_pos)
 	ray_query.to = ray_query.from + camera.project_ray_normal(mouse_pos) * 100
+	ray_query.collision_mask = 1 << CARD_LAYER - 1
 	var results = camera.get_world_3d().direct_space_state.intersect_ray(ray_query)
 
 	if results && results.collider.is_in_group("cards"):
