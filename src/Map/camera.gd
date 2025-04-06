b1ca2f28629f7acaa85aca0090d948723653bfc0
mbty <mbty@protonmail.com>
2025-04-06 11:26:27 +0200

    reactivate rail

--- src/Map/camera.gd
+++ src/Map/camera.gd
@@ -1,7 +1,8 @@
 extends Camera3D
 
 @export var margin = .2
-@export var speed = 1
+@export var pan_speed = 1
+@export var rail_speed = 1
 @export var amplitude_vt = 10  # degrees
 @export var amplitude_hz = 3  # degrees
 @export var center = Vector2(-30, 0)
@@ -26,8 +27,27 @@ func _physics_process(delta: float) -> void:
 		y_coeff = -pow(1.0 - (1.0 - y_ratio) / margin, 2.0)
 	x_coeff = clamp(x_coeff, -1.0, 1.0)
 	y_coeff = clamp(y_coeff, -1.0, 1.0)
-	rotation += speed * delta * Vector3(y_coeff, x_coeff, 0)
-	rotation = rotation.clamp(
-		Vector3(deg_to_rad(center.x - amplitude_vt), deg_to_rad(center.y - amplitude_hz), 0),
-		Vector3(deg_to_rad(center.x + amplitude_vt), deg_to_rad(center.y + amplitude_hz), 0)
-	)
+
+	if rail.progress_ratio == .01:
+		rotation += pan_speed * delta * Vector3(y_coeff, x_coeff, 0)
+		rotation = rotation.clamp(
+			Vector3(deg_to_rad(center.x - amplitude_vt), deg_to_rad(center.y - amplitude_hz), 0),
+			Vector3(deg_to_rad(center.x + amplitude_vt), deg_to_rad(center.y + amplitude_hz), 0)
+		)
+		if rotation.x > deg_to_rad(center.x + amplitude_vt - 1):
+			rail.progress_ratio == .011
+	else:
+		rotation += pan_speed * delta * Vector3(0, x_coeff, 0)
+		rotation = rotation.clamp(
+			Vector3(rotation.x, deg_to_rad(center.y - amplitude_hz), 0),
+			Vector3(rotation.x, deg_to_rad(center.y + amplitude_hz), 0)
+		)
+		if rail.progress_ratio < .99:
+			y_coeff = clamp(y_coeff, -1.0, 1.0)
+		else:
+			y_coeff = clamp(y_coeff, -1.0, 0)
+		rail.progress_ratio += rail_speed * delta * y_coeff
+		if rail.progress_ratio < 0.011:
+			rail.progress_ratio = 0.01
+		if rail.progress_ratio > .99:
+			rail.progress_ratio = .99
