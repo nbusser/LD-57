9e0767dcaf3cb306360a32ca480db6e407a66b36
Nicolas Busser <nicolas.busser67@gmail.com>
2025-04-06 17:45:49 +0900

    add cards manipulation methods

--- src/Map/card_hand.gd
+++ src/Map/card_hand.gd
@@ -8,16 +8,36 @@ const CARD_THICKNESS = 0.0003  # .3mm
 
 
 func spawn_cards(num_cards: int):
-	var angle_step = PI / 12
-	var total_angle = angle_step * num_cards
-	var start_angle = -total_angle / 2
-
 	for i in range(num_cards):
 		var card: Card = card_scene.instantiate()
 		var card_value = 1
 		card.init(card_value)
 		card.add_to_group("cards")
 		cards.add_child(card)
+	_reorder_cards()
+
+
+func add_card(card: Card, index: int):
+	cards.add_child(card)
+	cards.move_child(card, index)
+	_reorder_cards()
+
+
+func remove_card(index: int):
+	var card = cards.get_child(index)
+	cards.remove_child(card)
+	_reorder_cards()
+
+
+func _reorder_cards():
+	var num_cards = len(cards.get_children())
+
+	var angle_step = PI / 12
+	var total_angle = angle_step * num_cards
+	var start_angle = -total_angle / 2
+
+	for i in range(num_cards):
+		var card: Card = cards.get_child(i)
 
 		var angle = start_angle + (angle_step * i)
 		card.transform = card.transform.rotated_local(Vector3.LEFT, PI / 2)
