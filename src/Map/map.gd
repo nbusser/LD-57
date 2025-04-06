0f7465eacd5b1609f6a368436f6aad4418bfc9ea
Nicolas Busser <nicolas.busser67@gmail.com>
2025-04-06 13:14:05 +0900

    add card value variable

--- src/Map/map.gd
+++ src/Map/map.gd
@@ -5,7 +5,7 @@ const PLANE_COLLISION_LAYER = 6
 const CARD_THICKNESS = 0.0003  # .3mm
 const VERTICAL_OFFSET = Vector3.DOWN * 0.06
 
-var selected = null
+var selected: Card = null
 
 @onready var hand: Hand = $Hand
 @onready var fixed_arm: Node3D = $FixedArm
@@ -66,13 +66,14 @@ func spawn_cards(num_cards: int):
 	var start_angle = -total_angle / 2
 
 	for i in range(num_cards):
-		var card: Node3D = card_scene.instantiate()
-		card.init(1)
+		var card: Card = card_scene.instantiate()
+		var card_value = 1
+		card.init(card_value)
 		card.add_to_group("cards")
 		fixed_arm.add_child(card)
 
 		var angle = start_angle + (angle_step * i)
-		card.transform = card.transform.rotated_local(Vector3.LEFT, PI/2)
+		card.transform = card.transform.rotated_local(Vector3.LEFT, PI / 2)
 		card.transform = (
 			card
 			. transform
