30b05a19f73e242e41ab200d9dee69ff0de49041
Nicolas Busser <nicolas.busser67@gmail.com>
2025-04-06 19:37:22 +0900

    rename functions

--- src/Map/card_hand.gd
+++ src/Map/card_hand.gd
@@ -12,13 +12,13 @@ var _dragged_card: Card = null
 func grab_card_in_hand(card: Card):
 	_dragged_card = card
 	add_child(_dragged_card)
-	remove_card(card)
+	_hand_remove_card(card)
 
 
 func drop_card_in_hand(card: Card):
 	_dragged_card = null
 	remove_child(card)
-	add_card(card, 0)
+	_hand_add_card(card, 0)
 
 
 func spawn_cards(num_cards: int):
@@ -28,21 +28,20 @@ func spawn_cards(num_cards: int):
 		card.init(card_value)
 		card.add_to_group("cards")
 		cards_in_hand.add_child(card)
-	_reorder_cards()
+	_hand_reorder_cards()
 
 
-func add_card(card: Card, index: int):
+func _hand_add_card(card: Card, index: int):
 	cards_in_hand.add_child(card)
-	cards_in_hand.move_child(card, index)
-	_reorder_cards()
+	_hand_reorder_cards()
 
 
-func remove_card(card: Card):
+func _hand_remove_card(card: Card):
 	cards_in_hand.remove_child(card)
-	_reorder_cards()
+	_hand_reorder_cards()
 
 
-func _reorder_cards():
+func _hand_reorder_cards():
 	var num_cards = len(cards_in_hand.get_children())
 
 	var angle_step = PI / 12
