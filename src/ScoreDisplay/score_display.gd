ff968e26a46512f1be7f1fbe08c465ac76b71edb
Antoine Brûlé <antoine.brule@netc.eu>
2025-04-07 01:19:05 +0200

    improve score display

--- src/ScoreDisplay/score_display.gd
+++ src/ScoreDisplay/score_display.gd
@@ -2,6 +2,7 @@
 extends Node3D
 
 @onready var label_3d: Label3D = $Label3D
+@onready var points: Node3D = $Points
 
 @export_range(0, 20) var value = 20:
 	set(new_value):
@@ -10,8 +11,20 @@ extends Node3D
 
 
 func _ready():
+	var children = points.get_children()
+	for i in range(children.size()):
+		var point = children[i]
+		point.position = Vector3(i * 0.04, 0, 0)
+		point.color = Color.BLACK
 	_on_value_update()
 
 
 func _on_value_update():
 	label_3d.text = str(value)
+	var children = points.get_children()
+	for i in range(children.size()):
+		var point = children[i]
+		if i < value:
+			point.color = Color.WHITE
+		else:
+			point.color = Color.BLACK
