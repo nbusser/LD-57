bbc4a97f71ce41d43d59e17dc17ade5109b20b78
Nicolas Busser <nicolas.busser67@gmail.com>
2025-04-06 14:54:34 +0900

    change raycast to go to mouse pos

--- src/Map/finger_tip.gd
+++ src/Map/finger_tip.gd
@@ -3,14 +3,14 @@ extends Node2D
 @onready var camera = $"../../../../CameraRail/FollowRail/Camera"
 
 
-func get_closest_card():
-	var ray_query = PhysicsRayQueryParameters3D.new()
-	ray_query.from = camera.project_ray_origin(global_position)
-	ray_query.to = ray_query.from + camera.project_ray_normal(global_position) * 100
-	var results = camera.get_world_3d().direct_space_state.intersect_ray(ray_query)
+func get_closest_card() -> Card:
+	var mouse_pos = get_viewport().get_mouse_position()
 
-	var closest_card = null
-	var closest_distance = INF
+	var ray_query = PhysicsRayQueryParameters3D.new()
+	ray_query.from = camera.project_ray_origin(mouse_pos)
+	ray_query.to = ray_query.from + camera.project_ray_normal(mouse_pos) * 100
+	ray_query.collide_with_areas = true
+	var results = camera.get_world_3d().direct_space_state.intersect_ray(ray_query)
 
 	if results && results.collider.is_in_group("cards"):
 		return results.collider
