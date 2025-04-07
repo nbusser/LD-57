8e45e3d060657d14500ff41720533ac12fc8021d
Louis MOUHAT <louis.mouhat@gmail.com>
2025-04-08 01:50:38 +0200

    replace score tokens

--- src/ScoreDisplay/score_display_point.gd
+++ src/ScoreDisplay/score_display_point.gd
@@ -3,6 +3,6 @@ extends Node3D
 var color = Color.BLACK:
 	set(value):
 		color = value
-		light.material.emission = color
+		#light.material.emission = color
 
 @onready var light: CSGShape3D = $Light
