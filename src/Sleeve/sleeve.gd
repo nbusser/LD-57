402dfbbd2d3f9c9a851dd2c8ba7f797e9cf2e2f1
Nicolas Busser <nicolas.busser67@gmail.com>
2025-04-07 13:58:19 +0900

    externalize sleeve

---
+++ src/Sleeve/sleeve.gd
@@ -0,0 +1,16 @@
+class_name Sleeve extends Node2D
+
+signal finger_entered_sleeve
+signal finger_left_sleeve
+
+var finger_is_in_sleeve = false
+
+
+func _on_drop_zone_body_entered(_body: Node2D) -> void:
+	finger_is_in_sleeve = true
+	finger_entered_sleeve.emit()
+
+
+func _on_drop_zone_body_exited(_body: Node2D) -> void:
+	finger_is_in_sleeve = false
+	finger_left_sleeve.emit()
