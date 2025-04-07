251349cf2b1bb47f2eeb47a705f4113dbdc2cc9d
mbty <mbty@protonmail.com>
2025-04-07 12:56:39 +0200

    fix formatting

--- src/snapBattle/snap_battle.gd
+++ src/snapBattle/snap_battle.gd
@@ -9,13 +9,16 @@ var base_modulate = Color(0, 0, 0)
 @onready var base_texture: Texture2D = load("res://assets/sprites/battlefield.png")
 @onready var sprite = $Sprite3D
 
+
 func set_modulate(color):
 	base_modulate = color
 	if !glowing:
 		sprite.modulate = color
 
+
 func _ready() -> void:
 	$Sprite3D.texture = base_texture
+
 
 func _process(_delta: float) -> void:
 	var is_close = cards_manager.is_card_close_to_battlefield()
