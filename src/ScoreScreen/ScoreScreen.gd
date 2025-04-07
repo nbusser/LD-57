937c9b46168110a0f4bba18212fccfd5b0fc58c9
mbty <mbty@protonmail.com>
2025-04-07 11:32:29 +0200

    fix formatting

--- src/ScoreScreen/ScoreScreen.gd
+++ src/ScoreScreen/ScoreScreen.gd
@@ -8,14 +8,13 @@ var level_number: int
 
 
 func _ready() -> void:
-	assert(
-		level_number != null,
-		"init must be called before creating ScoreScreen scene"
-	)
+	assert(level_number != null, "init must be called before creating ScoreScreen scene")
 	level_label.text = str(level_number + 1)
+
 
 func init(level_number_p: int) -> void:
 	self.level_number = level_number_p
+
 
 func _on_NextLevelButton_pressed() -> void:
 	Globals.end_scene(Globals.EndSceneStatus.SCORE_SCREEN_NEXT)
