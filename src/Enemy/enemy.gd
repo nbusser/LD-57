5a2fa11c73bde0b9b69daae7ce4a0c06bc9cb073
Nicolas Busser <nicolas.busser67@gmail.com>
2025-04-08 10:08:51 +0900

    longer distract

--- src/Enemy/enemy.gd
+++ src/Enemy/enemy.gd
@@ -102,7 +102,7 @@ func _on_distraction_timer_timeout() -> void:
 	# Alien gets distracted only if he is idling
 	if state == Enums.EnemyState.IDLE:
 		var states = [
-			{state = Enums.EnemyState.DISTRACTED, distraction_time = 1.0},
+			{state = Enums.EnemyState.DISTRACTED, distraction_time = 2.5},
 			# Uncomment when we have a sleeping enemy
 			# {state = Enums.EnemyState.ASLEEP, distraction_time = 3.0}
 		]
